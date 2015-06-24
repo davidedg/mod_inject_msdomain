32eeec71c1a2602d4cbc5f04b035bf0376525ffa
Davide Del Grande <delgrande.davide@gmail.com>
2015-06-25 00:30:40 +0200

    Implemented Context-Aware Configuration and minor cleanups.

--- mod_inject_msdomain.c
+++ mod_inject_msdomain.c
@@ -13,34 +13,93 @@
 
 /*
  ==============================================================================
- Configuration
+    Configuration Structure
  ==============================================================================
  */
-static const char *domain;
 
-const char * inject_msdomain_set_domain(cmd_parms *cmd, void *cfg, const char *arg)
-{
-	domain = arg;
-    return NULL;
-}
+typedef struct {
+	char *domain;
+} inject_msdomain_config;
+
 
 /*
  ==============================================================================
- Configuration TABLE
+    Function Prototypes
  ==============================================================================
  */
+
+static int    my_proxyfixups(request_rec *r);
+static void* inject_msdomain_create_srv_conf(apr_pool_t* pool, server_rec* svr);
+static void* inject_msdomain_create_dir_conf(apr_pool_t* pool, char* context);
+static void   inject_msdomain_register_hooks(apr_pool_t *p);
+
+
+/*
+ ==============================================================================
+    Configuration Table
+ ==============================================================================
+ */
+
 static const command_rec directives[] =
 {
-    AP_INIT_TAKE1(
-	"InjectMSDomain" ,
-	inject_msdomain_set_domain,
-	NULL,
-	RSRC_CONF,
+	AP_INIT_TAKE1(
+	"InjectMSDomain",
+	ap_set_string_slot,
+	(void*)APR_OFFSETOF(inject_msdomain_config, domain),
+	OR_ALL,
 	"MS Domain to inject in Basic Authorization Header"
 	 ),
 
     {NULL}
 };
+
+
+/*
+ ==============================================================================
+    Module Name Tag
+ ==============================================================================
+ */
+
+module AP_MODULE_DECLARE_DATA inject_msdomain_module = {
+    STANDARD20_MODULE_STUFF,
+    inject_msdomain_create_dir_conf,    /* create per-dir    config structures */
+    NULL,                               /* merge  per-dir    config structures */
+    inject_msdomain_create_srv_conf,    /* create per-server config structures */
+    NULL,                               /* merge  per-server config structures */
+    directives,                         /* table of config file commands       */
+    inject_msdomain_register_hooks      /* register hooks                      */
+};
+
+/*
+ =======================================================================================================================
+    Hook registration
+ =======================================================================================================================
+ */
+
+static void inject_msdomain_register_hooks(apr_pool_t *p)
+{
+     ap_hook_fixups(my_proxyfixups, NULL, NULL, APR_HOOK_MIDDLE);
+}
+
+
+/*
+ =======================================================================================================================
+    Configuration Functions
+ =======================================================================================================================
+ */
+
+static void* inject_msdomain_create_srv_conf(apr_pool_t* pool, server_rec* svr) {
+  inject_msdomain_config* s = apr_pcalloc(pool, sizeof(inject_msdomain_config));
+  s->domain = NULL;
+  return s ;
+}
+
+static void* inject_msdomain_create_dir_conf(apr_pool_t* pool, char* context) {
+  inject_msdomain_config* d = apr_pcalloc(pool, sizeof(inject_msdomain_config));
+  d->domain = NULL;
+  return d ;
+}
+
 
 /*
  ==============================================================================
@@ -52,16 +111,14 @@ static int my_proxyfixups(request_rec *r)
 {
 	const char *t;
 	const char *sent_pw;
-	const char *check_header;
 	char *user;
 	char *d;
 	const char *auth_line;
 	char *AuthType = (PROXYREQ_PROXY == r->proxyreq) ? "Proxy-Authorization" : "Authorization";
+	
+	inject_msdomain_config *config = (inject_msdomain_config*) ap_get_module_config(r->per_dir_config, &inject_msdomain_module);
 
-	if (domain == NULL) {// return DECLINED; }
-		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0,r, "INJECT_MSDOMAIN: MISSING InjectMSDomain Directive");
-		return DECLINED;
-	}
+	if (config->domain == NULL) { return DECLINED; } // InjectMSDomain is empty
 	
     auth_line = apr_table_get(r->headers_in, AuthType);
 	if (!auth_line) { return DECLINED; }		// No AUTH from client
@@ -87,47 +144,32 @@ static int my_proxyfixups(request_rec *r)
 		if ((d-user) > 1) { // "DOMAIN\username"
 			return DECLINED;  // ignore if domain is already present
 		} else { // d-user == 1 ==> "\username" (some Androids)
-			ap_log_rerror(APLOG_MARK, APLOG_ERR, 0,r, "INJECT_MSDOMAIN: FIXING: %s to %s", user, user+1);
+			ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0,r, "INJECT_MSDOMAIN: FIXING: %s to %s", user, user+1);
 			user = user+1;
 		}
 	}
 		
-    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0,r, "INJECT_MSDOMAIN: CONVERTING: %s to %s\\%s", user, domain, user);
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0,r, "INJECT_MSDOMAIN: CONVERTING: %s to %s\\%s", user, config->domain, user);
 	
 	user = apr_pstrcat( r->pool, // INJECT MSDOMAIN IF NOT PRESENT
-		domain,
+		config->domain,
 		"\\",
 		user,
 		NULL);
 	
 	
-	auth_line = apr_pstrcat( r->pool, 	// Create new Authorization Header:
-	"Basic " , 			                // "Basic base64encore(domain\user:pass)"
+	auth_line = apr_pstrcat( r->pool,	// Create new Authorization Header:
+	"Basic ",							// "Basic base64encore(domain\user:pass)"
 	ap_pbase64encode( r->pool,
 		 apr_pstrcat( r->pool,
 			 user,
 			 ":", 		
-			 sent_pw , 	
+			 sent_pw, 	
 			 NULL))
 	, NULL);
+	
 	apr_table_set( r->headers_in , AuthType , auth_line );
 
     return OK;
 }
 
-
-
-static void inject_msdomain_register_hooks(apr_pool_t *p)
-{
-     ap_hook_fixups(my_proxyfixups, NULL, NULL, APR_HOOK_MIDDLE);
-}
-
-module AP_MODULE_DECLARE_DATA inject_msdomain_module = {
-    STANDARD20_MODULE_STUFF, 
-    NULL,                  /* create per-dir    config structures */
-    NULL,                  /* merge  per-dir    config structures */
-    NULL,                  /* create per-server config structures */
-    NULL,                  /* merge  per-server config structures */
-    directives,            /* table of config file commands       */
-    inject_msdomain_register_hooks  /* register hooks                      */
-};
